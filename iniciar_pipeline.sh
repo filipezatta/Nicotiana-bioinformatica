4f743e0dbb54d9637e91e10df45a32007db5a461
Filipe Zatta <filipe.fbz@gmail.com>
2026-05-28 22:25:28 +0000

    fix: resolve conflito de readme e blinda caminhos do docker no windows

--- iniciar_pipeline.sh
+++ iniciar_pipeline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Iniciando o Orquestrador Snakemake..."
-docker run -it --rm \
-  -v $(pwd):/workdir \
+
+MSYS_NO_PATHCONV=1 docker run -it --rm \
+  -v "$(pwd):/workdir" \
   -w /workdir \
   -v /var/run/docker.sock:/var/run/docker.sock \
   snakemake/snakemake:v7.32.4 bash -c "./setup_matrix.sh && snakemake --cores all -s workflow/Snakefile"
