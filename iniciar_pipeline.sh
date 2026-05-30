68baf36730a39a4776426baa9b201804e698c32d
Filipe Zatta <filipe.fbz@gmail.com>
2026-05-30 15:14:51 +0000

    fix: substitui apt-get quebrado por binario estatico do docker cli

--- iniciar_pipeline.sh
+++ iniciar_pipeline.sh
@@ -5,4 +5,4 @@ MSYS_NO_PATHCONV=1 docker run -it --rm \
   -v "$(pwd):/workdir" \
   -w /workdir \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  snakemake/snakemake:v7.32.4 bash -c "apt-get update && apt-get install -y docker.io && ./setup_matrix.sh && snakemake --cores all -s workflow/Snakefile"
+  snakemake/snakemake:v7.32.4 bash -c "curl -sSL https://download.docker.com/linux/static/stable/x86_64/docker-24.0.9.tgz | tar -xzC /tmp/ && mv /tmp/docker/docker /usr/bin/ && ./setup_matrix.sh && snakemake --cores all -s workflow/Snakefile"
