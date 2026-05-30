ffb22c996ca3e2b735fa88ce15606ded8b7c9078
Filipe Zatta <filipe.fbz@gmail.com>
2026-05-30 15:09:50 +0000

    fix: instala docker cli dentro do conteiner snakemake para resolver command not found

--- iniciar_pipeline.sh
+++ iniciar_pipeline.sh
@@ -5,4 +5,4 @@ MSYS_NO_PATHCONV=1 docker run -it --rm \
   -v "$(pwd):/workdir" \
   -w /workdir \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  snakemake/snakemake:v7.32.4 bash -c "./setup_matrix.sh && snakemake --cores all -s workflow/Snakefile"
+  snakemake/snakemake:v7.32.4 bash -c "apt-get update && apt-get install -y docker.io && ./setup_matrix.sh && snakemake --cores all -s workflow/Snakefile"
